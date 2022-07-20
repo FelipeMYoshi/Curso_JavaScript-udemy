1c869c7a294f3b843d354dc723a5bf913d2e6c6a
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-20 16:19:13 -0300

    Adicionando coemntário

---
+++ fundamentos/início/sentenca_2.js
@@ -0,0 +1,6 @@
+console.log("teste");
+console.log('Passo #01');
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');
+//Adiciona comentário para auxiliar na descrição ou leitura do codigo
