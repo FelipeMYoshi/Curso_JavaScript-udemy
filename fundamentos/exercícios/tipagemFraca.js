5788555f747af25679f23dacaf9002ca90bc319a
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-21 09:54:49 -0300

    Javascript é uma linguagem fracamente tipada, não exige declarar os tipos de dados para executar

---
+++ fundamentos/exercícios/tipagemFraca.js
@@ -0,0 +1,12 @@
+let qualquer = "Legal";
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1425;
+console.log(qualquer);
+console.log(typeof qualquer);
+
+// Evitar nome genérico e siglas
+let valor = "";
+let numero = 1;
+let pqp = "Produto quimico perigoso"
