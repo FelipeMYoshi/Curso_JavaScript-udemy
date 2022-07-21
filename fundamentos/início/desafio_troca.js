2cc35389a798b901124dee519f8bafcacca547c6
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-20 22:41:16 -0300

    Desafio como trocar valores em variáveis

---
+++ fundamentos/início/desafio_troca.js
@@ -0,0 +1,22 @@
+//Desafio é trocar os valores das variáveis
+let a = 7;
+let b = 94;
+console.log(a);
+console.log(b);
+//O resultados deve ser 
+//a = 94
+//b = 7
+
+//Ex do prof
+[a, b] = [b, a]
+
+let troca = a;
+
+//a = b;
+//b = troca;
+console.log(a);
+console.log(b);
+
+
+
+
