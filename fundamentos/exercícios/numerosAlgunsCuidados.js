e090b240fac2fc54804a46c13a685482cf37154e
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-21 10:21:55 -0300

    Algumas curiosidades relacionada as dados numéricos em JS de imprecisão e resultado infinito

---
+++ fundamentos/exercícios/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0); // No JS alguns tipos de calculos com resultado infinito retorna declarado "Infinity"
+console.log("10" / 2); // O js entende o valor numerico dentro de uma string
+console.log("Show!" * 2); // Não entende com palavras
+console.log(0.1 + 0.7); // alguns calculos terão imprecisão 
+// console.log(10.toString()) //Não executa
+console.log((10.345).toFixed(2))
