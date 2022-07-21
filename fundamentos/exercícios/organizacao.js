02c50de93fa53f242621f4af7be18f4f07a76a8d
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-20 23:11:52 -0300

    Breve explanação sobre organização do JS

---
+++ fundamentos/exercícios/organizacao.js
@@ -0,0 +1,13 @@
+//Sentença
+console.log("Sentença de código");
+
+//Bloco
+{
+    {
+        console.log("Olá");
+        console.log("Mundo!");
+    }
+}
+
+//JS é organizado em sequência de código que podem ou não terminar em ponto e vírgula
+//e também por blocos de códigos delimitados por chaves {}
