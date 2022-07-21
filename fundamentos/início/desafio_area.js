79a5170562f6a14660bb8eda3cab548c1c80a4a7
Felipe Martins Yoshimoto <felipemyoshimoto@hotmail.com>
2022-07-20 22:18:34 -0300

    Desafio de calculo de área de circunferência (PI * área * área)

---
+++ fundamentos/início/desafio_area.js
@@ -0,0 +1,11 @@
+// PI * raio * raio
+
+//Minha resposta
+const PI = 3.141592;
+let raio = 10;
+let valorRaio = PI * raio * raio
+console.log("O valore da área é : " + valorRaio);
+
+//Resposta do prof
+console.log("O valor da área é: " + Math.PI * (raio * raio) + "m2.")
+//Existe o valor disponível de PI do JS em Math.
